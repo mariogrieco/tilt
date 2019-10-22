a0b156ec96a831ea6b1aed90b2070cd432c03db8
Francisco Rubial <franjorub@gmail.com>
2019-10-22 18:43:35 -0400

    allow url previews only for https urls

--- App/components/Post/Post.js
+++ App/components/Post/Post.js
@@ -423,7 +423,7 @@ class Post extends React.Component {
     const files =
       metadata && metadata.files && metadata.files ? metadata.files : [];
 
-    const REGEX = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/g;
+    const REGEX = /(https:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
     const hasUrlForPreview = Boolean(
       message && message.match(REGEX) && message.match(REGEX)[0],
     );
