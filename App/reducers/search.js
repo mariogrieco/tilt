e0ceca1a412945060f9499f76c0d16586b6b5fdb
griecomv <mariojosuexz@gmail.com>
2019-10-25 19:37:31 -0400

    adding convention to search , and input

--- App/reducers/search.js
+++ App/reducers/search.js
@@ -3,12 +3,14 @@ import {
   SEARCH_CHANNELS_SUCCESS,
 } from '../actions/search';
 
+import fix_name_if_need from '../utils/fix_name_if_need';
+
 const initialState = [];
 
 export default (state = initialState, {type, payload}) => {
   switch (type) {
     case SEARCH_CHANNELS_SUCCESS: {
-      return payload;
+      return payload.map(channel => fix_name_if_need(channel));
     }
     default: {
       return state;
