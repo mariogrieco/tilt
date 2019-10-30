245f549448f5ec197d16ca7b715f11553937693e
griecomv <mariojosuexz@gmail.com>
2019-10-30 15:43:57 -0400

    fixed problems on loading

--- App/actions/HashtagChannelsPaginator.js
+++ App/actions/HashtagChannelsPaginator.js
@@ -1,5 +1,7 @@
 import Client4 from '../api/MattermostClient';
 
+import {GET_CHANNELS_SUCESS} from './channels';
+
 export const GET_PAGE_SUCCESS = 'GET_PAGE_SUCCESS';
 export const GET_PAGE_ERROR = 'GET_PAGE_ERROR';
 
@@ -7,6 +9,20 @@ export const getHashtagChannels = page => async dispatch => {
   try {
     const channels = await Client4.getHashtagChannels(page, 50);
     dispatch(getHashtagChannelsSucess({channels, page: ++page}));
+    return channels;
+  } catch (ex) {
+    dispatch(getHashtagChannelsError(ex));
+    return Promise.reject(ex.message);
+  }
+};
+
+export const getPageOnFocus = () => async dispatch => {
+  try {
+    const channels = await Client4.getHashtagChannels(0, 50);
+    dispatch({
+      type: GET_CHANNELS_SUCESS,
+      payload: channels,
+    });
     return channels;
   } catch (ex) {
     dispatch(getHashtagChannelsError(ex));
--- App/components/Discover/index.js
+++ App/components/Discover/index.js
@@ -2,10 +2,14 @@ import React, {Fragment} from 'react';
 import {connect} from 'react-redux';
 import {FlatList, ActivityIndicator, Platform} from 'react-native';
 import isEqual from 'lodash/isEqual';
+import {withNavigation} from 'react-navigation';
 import getJoinChannelsList from '../../selectors/getJoinChannelsList';
 import ChannelDisplayName from '../ChannelDisplayName';
 // import {setChannelPaginator, getChannels} from '../../actions/channels';
-import {getHashtagChannels} from '../../actions/HashtagChannelsPaginator';
+import {
+  getHashtagChannels,
+  getPageOnFocus,
+} from '../../actions/HashtagChannelsPaginator';
 
 class Discover extends React.Component {
   state = {
@@ -16,11 +20,23 @@ class Discover extends React.Component {
     return !isEqual(nextProps, this.props) || !isEqual(nextState, this.state);
   }
 
-  _fetchMore = ({distanceFromEnd}) => {
-    if (distanceFromEnd > 0) {
-      return false;
+  componentWillUnmount() {
+    if (this.navigationListener) {
+      this.navigationListener.remove();
     }
+  }
 
+  componentDidMount() {
+    const {navigation} = this.props;
+
+    this.navigationListener = navigation.addListener('didFocus', () => {
+      this.props.getPageOnFocus();
+    });
+    this.props.getPageOnFocus();
+  }
+
+  _fetchMore = ({distanceFromEnd}) => {
+    if (distanceFromEnd < 0) return null;
     this.setState(
       {
         loading: true,
@@ -44,10 +60,6 @@ class Discover extends React.Component {
     );
   };
 
-  componentDidMount() {
-    this._fetchMore({distanceFromEnd: 0});
-  }
-
   renderItem({item}) {
     return (
       <ChannelDisplayName
@@ -65,6 +77,7 @@ class Discover extends React.Component {
   render() {
     const {channels} = this.props;
     const {loading} = this.state;
+    console.log(channels.length);
     return (
       <Fragment>
         <FlatList
@@ -73,12 +86,12 @@ class Discover extends React.Component {
           extraData={channels}
           keyExtractor={channel => channel.id}
           renderItem={this.renderItem}
-          viewabilityConfig={{viewAreaCoveragePercentThreshold: 35}}
-          initialNumToRender={22}
+          viewabilityConfig={{viewAreaCoveragePercentThreshold: 0.15}}
+          initialNumToRender={50}
           onEndReached={this._fetchMore}
-          onEndReachedThreshold={0}
+          onEndReachedThreshold={0.15}
           keyboardDismissMode="on-drag"
-          updateCellsBatchingPeriod={150}
+          updateCellsBatchingPerio={150}
           maxToRenderPerBatch={5}
           removeClippedSubviews={Platform.OS === 'android'}
         />
@@ -94,7 +107,9 @@ const mapStateToProps = state => ({
   stop: state.hashtagChannelsPaginator.stop,
 });
 
-export default connect(
-  mapStateToProps,
-  {getHashtagChannels},
-)(Discover);
+export default withNavigation(
+  connect(
+    mapStateToProps,
+    {getHashtagChannels, getPageOnFocus},
+  )(Discover),
+);
