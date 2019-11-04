e2e027d79a85e974148193c5a52f86a45f525598
Andre Norman <andrenorman1@gmail.com>
2019-11-04 15:31:05 -0600

    Update keyboardHelper.js

--- App/utils/keyboardHelper.js
+++ App/utils/keyboardHelper.js
@@ -1,21 +1,20 @@
-import { Platform } from 'react-native';
-import { ifIphoneX } from 'react-native-iphone-x-helper';
+import {Platform} from 'react-native';
+import {ifIphoneX} from 'react-native-iphone-x-helper';
 
 const isIOS = Platform.OS === 'ios';
 const behavior = isIOS ? 'position' : '';
 
-
 export const channelTab = {
   offset: ifIphoneX(136, 108),
-  behavior
+  behavior,
 };
 
 export const channelScreen = {
   offset: ifIphoneX(88, 60),
-  behavior
+  behavior,
 };
 
 export default {
   channelScreen,
-  channelTab
+  channelTab,
 };
