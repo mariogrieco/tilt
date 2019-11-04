8b2bd628742672f985955c32d1e7563bddf56dbe
griecomv <mariojosuexz@gmail.com>
2019-11-04 17:16:55 -0400

    fixed ios

--- App/utils/keyboardHelper.js
+++ App/utils/keyboardHelper.js
@@ -2,20 +2,21 @@ import { Platform } from 'react-native';
 import { ifIphoneX } from 'react-native-iphone-x-helper';
 
 const isIOS = Platform.OS === 'ios';
-const behavior = isIOS ? 'padding' : 'padding';
+const behavior = isIOS ? 'position' : 'padding';
 
 
 export const channelScreen = {
-  offset: ifIphoneX(-160, -160),
-  behavior
+  offset: ifIphoneX(136, -160),
+  behavior,
 };
 
 export const channelTab = {
-  offset: ifIphoneX(-110, -110),
-  behavior
+  offset: ifIphoneX(88, -110),
+  behavior,
 };
 
 export default {
   channelScreen,
   channelTab
 };
+
