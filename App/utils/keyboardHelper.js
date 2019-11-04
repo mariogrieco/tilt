d009cdbdc18d3c20534fa6f21f58d7d270dd195d
griecomv <mariojosuexz@gmail.com>
2019-11-04 17:23:39 -0400

    adding ios ternari

--- App/utils/keyboardHelper.js
+++ App/utils/keyboardHelper.js
@@ -1,22 +1,21 @@
-import { Platform } from 'react-native';
-import { ifIphoneX } from 'react-native-iphone-x-helper';
+
+import {Platform} from 'react-native';
+import {ifIphoneX} from 'react-native-iphone-x-helper';
 
 const isIOS = Platform.OS === 'ios';
 const behavior = isIOS ? 'position' : 'padding';
 
-
 export const channelScreen = {
-  offset: ifIphoneX(136, -160),
+  offset: isIOS ? ifIphoneX(136, -108) : -160,
   behavior,
 };
 
 export const channelTab = {
-  offset: ifIphoneX(88, -110),
+  offset: isIOS ? ifIphoneX(88, 60) : -110,
   behavior,
 };
 
 export default {
   channelScreen,
-  channelTab
+  channelTab,
 };
-
