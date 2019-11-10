97184040eff746ef46e012121c655675ab933ca0
anorman <andrenorman1@gmail.com>
2019-11-10 00:11:58 -0500

    Update index.js

--- App/components/SeparatorContainer/index.js
+++ App/components/SeparatorContainer/index.js
@@ -4,17 +4,19 @@ import moment from 'moment';
 import Separator from '../Separator';
 
 import styles from './styles';
+import {connect} from 'react-redux';
 
 export class SeparatorContainer extends PureComponent {
   render() {
     const {createdAt, noPadding} = this.props;
+    const {theme} = this.props;
     return (
       <View style={[styles.separator, noPadding ? {padding: 0} : {}]}>
         <View style={{flex: 1}}>
           <Separator />
         </View>
         <View style={{flex: 1, alignItems: 'center'}}>
-          <Text style={styles.separatorText}>
+          <Text style={[styles.separatorText, {color: theme.primaryTextColor}]}>
             {createdAt ? moment(createdAt).format('ddd, MMM D, YYYY') : 'Today'}
           </Text>
         </View>
@@ -26,4 +28,5 @@ export class SeparatorContainer extends PureComponent {
   }
 }
 
-export default SeparatorContainer;
+const mapStateToProps = ({themes}) => ({theme: themes[themes.current]});
+export default connect(mapStateToProps)(SeparatorContainer);
