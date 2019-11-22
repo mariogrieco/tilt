9510747283651f18a21a3757ba24bb695969885e
griecomv <mariojosuexz@gmail.com>
2019-11-22 13:01:19 -0400

    css details

--- App/components/chart-modal/styles.js
+++ App/components/chart-modal/styles.js
@@ -3,7 +3,7 @@ import StyleSheet from 'react-native-extended-stylesheet';
 export default StyleSheet.create({
   container: {
     marginHorizontal: 5,
-    marginVertical: 45,
+    marginVertical: 85,
     borderRadius: 8,
     overflow: 'hidden',
     flex: 1,
