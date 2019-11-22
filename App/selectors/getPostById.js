1d1ed655cc56970154968c8fd8a0f3ef6f15d0c1
Francisco Rubial <franjorub@gmail.com>
2019-11-22 15:49:40 -0400

    fixed post data in modal actions

--- App/selectors/getPostById.js
+++ App/selectors/getPostById.js
@@ -1,8 +1,10 @@
 import cloneDeep from 'lodash/cloneDeep';
 
 const getPostById = (state, postId) => {
-  const post = state.posts.entities[postId];
-  if (!post) return {};
+  const post = state.posts.entities[postId] || state.feeds.posts[postId];
+  if (!post) {
+    return {};
+  }
   return {
     ...post,
     user: cloneDeep(state.users.data[post.user_id]) || {},
