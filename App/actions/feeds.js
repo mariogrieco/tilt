e682caf0a3ab2142de7939803bfc8bbbe0116922
Francisco Rubial <franjorub@gmail.com>
2019-11-23 14:39:23 -0400

    fixed date order for feeds

--- App/actions/feeds.js
+++ App/actions/feeds.js
@@ -1,12 +1,22 @@
+import orderBy from 'lodash/orderBy';
+
 export const GET_FEEDS_SUCCESS = 'GET_FEEDS_SUCCESS';
 
 export const getFeeds = () => async dispatch => {
   try {
     const response = await fetch('https://staging.tiltchat.com/services/feed');
     const data = await response.json();
+    const postsSortedByDate = orderBy(
+      data.posts_keys,
+      [key => data.posts[key].create_at],
+      ['desc'],
+    );
     dispatch({
       type: GET_FEEDS_SUCCESS,
-      payload: data,
+      payload: {
+        ...data,
+        posts_keys: postsSortedByDate,
+      },
     });
   } catch (err) {
     console.log(err);
